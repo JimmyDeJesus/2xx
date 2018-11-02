6689ba5fabfb492801c392a5a571159c52b48074
JimmyDeJesus <jimmydejesus101@gmail.com>
2018-11-02 19:55:58 -0400

    added constant variables and updated sitename variables with constant

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+	// Pulls data information and configuration used through out the application.
+		require_once 'config.inc.php';
+
 	// Pulls data elements used through out the entier website
 		require_once 'content.data.php';
 
@@ -12,9 +15,6 @@
 		 JavaScript variables the same names used for PHP
 
 	*/
-
-$siteName = 'inside out | 2xx';
-
 ?>
 
 <!doctype html>
@@ -33,7 +33,7 @@ $siteName = 'inside out | 2xx';
                for the browser, search engines, etc.
           */
           ?>
-          <title><?php echo $siteName; ?></title>
+          <title><?php echo SITENAME; ?></title>
           <?php
           /*
                used to display information about the
@@ -44,7 +44,7 @@ $siteName = 'inside out | 2xx';
           <meta name="description" content="inside out is a class project we use to learn html."/>
           <link rel="stylesheet" type="text/css" href="assets/css/style.css">
           <script>
-               var siteName = '<?php echo $siteName; ?>';
+               var siteName = '<?php echo SITENAME; ?>';
           </script>
      </head>
      <?php
@@ -52,7 +52,7 @@ $siteName = 'inside out | 2xx';
      ?>
      <body>
           <header>
-               <h1><a href="index.php"><?php echo $siteName; ?></a></h1>
+               <h1><a href="index.php"><?php echo SITENAME; ?></a></h1>
                <h2>continuously falling forward in to the light...</h2>
 			  <nav>
 				  <?php echo menuBuilder($content['pages']); ?>
