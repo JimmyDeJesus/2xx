6bd68b9db6a74504f6e4c580dc825580151b7fd5
JimmyDeJesus <jimmydejesus101@gmail.com>
2018-11-02 20:11:55 -0400

    added copyright and versioning

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -3,6 +3,7 @@
                <h4>this is the footer</h4>
                <p>we place redundant information here</p>
                <div id="footerTagLine">I am 7 inches left of normal</div>
+                <div class="copyright">iop project - Version <?php echo VERSION; ?> | &copy; 1990 - <?php echo date('Y'); ?> Jimmy De Jesus</div>
           </footer>
      </body>
 </html>
