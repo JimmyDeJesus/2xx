35ecb74c1366ae21c6a35ef6f26c3fae8455cafa
JimmyDeJesus <jimmydejesus101@gmail.com>
2018-11-30 21:07:41 -0500

    updates to functions and index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-	
-	require_once('include/header.inc.php');
-?>
-          <section>
-               <p>this is where the page content goes</p>
-          </section>
 
-<?php
+	require_once('include/header.inc.php');
+
+	getPage($content['pages']);
 
 	require_once('include/footer.inc.php');
 
